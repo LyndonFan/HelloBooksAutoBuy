3ba7b80e4f37afd7d0965dad3341a5061bc9c281
Lyndon Fan <lyndon0808@gmail.com>
2023-02-19 19:21:24 +0000

    black format

--- get_links.py
+++ get_links.py
@@ -7,12 +7,12 @@ CWD = os.path.dirname(os.path.abspath(__file__))
 
 
 def find_links(filename):
-    with open(filename, 'r') as f:
-        soup = BeautifulSoup(f.read(), 'html.parser')
+    with open(filename, "r") as f:
+        soup = BeautifulSoup(f.read(), "html.parser")
 
-    elements = soup.find_all('a')
-    links = [e['href'] for e in elements]
-    links = set(l for l in links if 'geni.us/' in l)
+    elements = soup.find_all("a")
+    links = [e["href"] for e in elements]
+    links = set(l for l in links if "geni.us/" in l)
     return links
 
 
@@ -20,13 +20,13 @@ def get_all_links():
     dirs = os.listdir(CWD)
     links = set()
     for d in dirs:
-        if 'Hello_Books__' in d:
-            fname = os.path.join(CWD, d, 'index.html')
+        if "Hello_Books__" in d:
+            fname = os.path.join(CWD, d, "index.html")
             links.update(find_links(fname))
     return links
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     links = get_all_links()
-    with open('links.txt', 'w+') as f:
-        f.write('\n'.join(links))
+    with open("links.txt", "w+") as f:
+        f.write("\n".join(links))
