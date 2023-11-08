8a7d0c4f8407e6465568c707e878648027d81aa8
Lyndon Fan <lyndon0808@gmail.com>
2023-11-08 01:22:38 +0000

    nit: manual verification, redirect to uk

--- buy_products.py
+++ buy_products.py
@@ -21,7 +21,11 @@ def sign_in(driver):
         "https://www.amazon.co.uk/ap/signin?openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.return_to=https%3A%2F%2Fwww.amazon.co.uk%2Fref%3Dgw_sgn_ib%3F_encoding%3DUTF8%26pf_rd_p%3D644cc3df-edda-483a-b5b9-37ecf58fdf0e%26pd_rd_wg%3Do5prS%26pf_rd_r%3D05RBYQ8ZEMKJ2VNG4A0C%26content-id%3Damzn1.sym.644cc3df-edda-483a-b5b9-37ecf58fdf0e%26pd_rd_w%3DTx3RV%26pd_rd_r%3D5924fccb-3bf1-4684-9649-243f69a430d0&openid.assoc_handle=gbflex&openid.pape.max_auth_age=0&pf_rd_r=NN2BSD96XARTK2R8G5DK&pf_rd_p=644cc3df-edda-483a-b5b9-37ecf58fdf0e&pd_rd_r=8d8f9af0-0c11-4529-a2bf-e8beba1a6779&pd_rd_w=GxRqW&pd_rd_wg=Tpshg&ref_=pd_gw_unk"
     )
     time.sleep(1)
-    email_field = driver.find_element("xpath", '//input[@id="ap_email"]')
+    try:
+        email_field = driver.find_element("xpath", '//input[@id="ap_email"]')
+    except Exception as e:
+        print(e)
+        wait = input("Something went wrong. Fix it and press enter to continue")
     email = os.environ["EMAIL"]
     email_field.send_keys(email)
     continue_button = driver.find_element("xpath", '//input[@id="continue"]')
@@ -41,9 +45,10 @@ def buy_product(driver, url):
     time.sleep(1)
     # old url only redirects to amazon US, have to change to UK for it to remember account
     current_url = driver.current_url
-    new_url = current_url.replace("https://www.amazon.com/", "https://www.amazon.co.uk/")
-    driver.get(new_url)
-    time.sleep(1)
+    if not "amazon.co.uk" in current_url:
+        new_url = current_url.replace("https://www.amazon.com/", "https://www.amazon.co.uk/")
+        driver.get(new_url)
+        time.sleep(1)
     title = driver.find_element("xpath", '//span[@id="productTitle"]').text
     price = driver.find_element("xpath", '//span[@id="kindle-price"]').text
     print(f"{title}: {price}")
