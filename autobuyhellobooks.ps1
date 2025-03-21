e1ecae9b0d1977a5eea216254ba3a332d76775f1
LyndonFan <lyndon0808@gmail.com>
2025-03-21 17:49:28 +0000

    update to stop sleeping while script is running

--- autobuyhellobooks.ps1
+++ autobuyhellobooks.ps1
@@ -1,1 +1,7 @@
+$alwaysOn = (powercfg /list | Select-String -Pattern "GUID:.+[0-9a-f\-]+.+\(AlwaysOn\)").Matches[0].Value -replace ".*GUID: " -replace " +\(.*"
+$balanced = (powercfg /list | Select-String -Pattern "GUID:.+[0-9a-f\-]+.+\(Balanced\)").Matches[0].Value -replace ".*GUID: " -replace " +\(.*"
+Write-Host "Switching power plan to AlwaysOn: $alwaysOn"
+powercfg /setactive $alwaysOn
 uv run python main.py
+Write-Host "Switching power plan to Balanced: $balanced"
+powercfg /setactive $balanced
