b211f593548895adc158c31a64664f4e325cef74
LyndonFan <lyndon0808@gmail.com>
2025-04-04 19:07:12 +0100

    cds into current folder first

--- autobuyhellobooks.ps1
+++ autobuyhellobooks.ps1
@@ -2,6 +2,8 @@ $alwaysOn = (powercfg /list | Select-String -Pattern "GUID:.+[0-9a-f\-]+.+\(Alwa
 $balanced = (powercfg /list | Select-String -Pattern "GUID:.+[0-9a-f\-]+.+\(Balanced\)").Matches[0].Value -replace ".*GUID: " -replace " +\(.*"
 Write-Host "Switching power plan to AlwaysOn: $alwaysOn"
 powercfg /setactive $alwaysOn
+$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+Set-Location $scriptDir
 uv run python main.py
 Write-Host "Switching power plan to Balanced: $balanced"
 powercfg /setactive $balanced
